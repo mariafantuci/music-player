18e01b18ad7c35087ec6cec43243ba4bcd0594eb
mahfantuci <maria@imaginationmedia.com>
2023-02-08 20:59:33 -0300

    Adding more color and add images icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,15 @@ module.exports = {
     extend: {
       colors: {
 				purple: "#2A2141",
+        "light-purple": "#5E586F",
         "light-gray": "#E1E1E6",
+        "black-bg": "#0F0D13",
+        "light-gray-100": "#C4C4CC",
+        "light-gray-200": "#D9D9D9",
 			},
 			fontFamily: {
 				roboto: ["Roboto", "sans-serif"],
-			},
+			}
     },
   },
   plugins: [],
